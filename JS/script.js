7dc5c5ddac914ce43b10f3e927d8093221fb732f
Rabei David <rabeidavid2006@gmail.com>
2026-03-26 09:28:17 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,77 @@
+function setTema(t){
+document.body.classList.remove("gradient","light","dark");
+document.body.classList.add(t);
+}
+
+function rate(n){
+alert("Ai dat rating " + n + " stele!");
+}
+
+function like(){
+alert("Ai adăugat la favorite ❤️");
+}
+
+/* COS */
+function addToCart(nume){
+
+let cos = JSON.parse(localStorage.getItem("cos")) || [];
+
+cos.push(nume);
+
+localStorage.setItem("cos", JSON.stringify(cos));
+
+alert(nume + " adăugat în coș 🛒");
+
+afiseazaCos();
+}
+
+function afiseazaCos(){
+
+let lista = document.getElementById("cos-lista");
+if(!lista) return;
+
+let cos = JSON.parse(localStorage.getItem("cos")) || [];
+
+lista.innerHTML="";
+
+cos.forEach((item,index)=>{
+let li=document.createElement("li");
+li.innerHTML=item + ` <button onclick="stergeDinCos(${index})">❌</button>`;
+lista.appendChild(li);
+});
+}
+
+function stergeDinCos(index){
+
+let cos = JSON.parse(localStorage.getItem("cos")) || [];
+
+cos.splice(index,1);
+
+localStorage.setItem("cos", JSON.stringify(cos));
+
+afiseazaCos();
+}
+
+/* RANDOM */
+function randomParfum(){
+
+let parfumuri=[
+"parfumuri/lebeau.html",
+"parfumuri/sauvage.html",
+"parfumuri/eros.html",
+"parfumuri/khamrah.html",
+"parfumuri/maahir.html",
+"parfumuri/erosenergy.html",
+"parfumuri/najim.html",
+"parfumuri/powerfully.html",
+"parfumuri/imagination.html",
+"parfumuri/oudwood.html",
+"parfumuri/armani.html"
+];
+
+let random=Math.floor(Math.random()*parfumuri.length);
+
+window.location.href=parfumuri[random];
+}
+
+window.addEventListener("DOMContentLoaded", afiseazaCos);
